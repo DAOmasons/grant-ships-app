24fa8c5ee13d95d150b6202e8201dd7ceacb272a
jordan <jordan.lesich@gmail.com>
2024-01-21 16:31:39 -0800

    address, add validation, highlight, and delete functionality

--- src/components/AddressBox.tsx
+++ src/components/AddressBox.tsx
@@ -1,5 +1,8 @@
 import {
+  ActionIcon,
+  CloseIcon,
   Flex,
+  Group,
   Highlight,
   MantineComponent,
   Mark,
@@ -11,54 +14,76 @@ import {
   __InputStylesNames,
   useMantineTheme,
 } from '@mantine/core';
-import React, { forwardRef, useState } from 'react';
+import { IconSquareRoundedX, IconX } from '@tabler/icons-react';
+import React, { forwardRef, useMemo, useState } from 'react';
 import { isAddress } from 'viem';
 
 export const AddressBox = (props: TextareaProps) => {
   const theme = useMantineTheme();
-  const [inputData, setInputData] = useState<string[]>([]);
+  const [value, setValue] = useState('');
+  //   const [addrData, setAddrData] = useState<string[]>([]);
 
   //   const [hasInvalidAddress, setHasInvalidAddress] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const pastedData = e.target.value;
-
-    const addresses = pastedData
-      .split(',')
-      .filter(Boolean)
-      .map((addr) => addr.trim());
-
-    console.log('addresses', addresses);
-
-    setInputData(addresses);
+    setValue(e.target.value);
   };
 
-  const nonValidAddresses = inputData.filter((addr) => !isAddress(addr));
+  const addrData = useMemo(() => {
+    const addresses = value
+      .split(',')
+      .map((addr) => addr.trim())
+      .filter(Boolean);
+
+    console.log('addresses', addresses);
+    return addresses;
+  }, [value]);
+
+  const nonValidAddresses = addrData.filter((addr) => !isAddress(addr));
   const errorMessage = nonValidAddresses.length
     ? `Has ${nonValidAddresses?.length} Invalid ${
         nonValidAddresses?.length === 1 ? 'Address' : 'Addresses'
       }`
     : props.error;
+
+  const deleteAddress = (addr: string) => {
+    setValue(value.replace(addr, ''));
+  };
   return (
     <>
-      <Textarea {...props} onChange={handleChange} error={errorMessage} />
+      <Textarea
+        {...props}
+        onChange={handleChange}
+        error={errorMessage}
+        value={value}
+      />
       <Flex w="100%" direction="column">
-        {inputData.map((addr, index) => (
+        {addrData.map((addr, index) => (
           <Paper
             bg={theme.colors.dark[5]}
             w="fit-content"
             key={addr + index}
             mb={'xs'}
+            p={4}
           >
-            <Text
-              p={4}
-              component="a"
-              href=""
-              size="xs"
-              c={theme.colors.blue[5]}
-            >
-              {isAddress(addr) ? addr : <Mark color="red">{addr}</Mark>}
-            </Text>
+            <Group align="center">
+              <Text
+                component="a"
+                href=""
+                size="xs"
+                c={theme.colors.blue[5]}
+                style={{ transform: 'translateY(1px)' }}
+              >
+                {isAddress(addr) ? addr : <Mark color="red">{addr}</Mark>}
+              </Text>
+              <ActionIcon
+                variant="subtle"
+                size="xs"
+                onClick={() => deleteAddress(addr)}
+              >
+                <IconX size={12} />
+              </ActionIcon>
+            </Group>
           </Paper>
         ))}
       </Flex>
