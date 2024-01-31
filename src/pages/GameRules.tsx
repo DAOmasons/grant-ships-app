5656dbe435e55bff00dd1086db053a62ebbc1715
jordan <jordan.lesich@gmail.com>
2024-01-30 19:22:15 -0800

    setup page structure

---
+++ src/pages/GameRules.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const GameRules = () => {
+  return <div>GameRules</div>;
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,18 @@ import { Home } from './pages/Home';
 import { Projects } from './pages/Projects';
 import { Project } from './pages/Project';
 import { RegisterProject } from './components/forms/RegisterProject';
+import { Ships } from './pages/Ships';
+import { GameRules } from './pages/GameRules';
+import { Apply } from './pages/Apply';
 
 export const ClientRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="projects" element={<Projects />} />
-    <Route path="project/:id" element={<Project />} />
     <Route path="register-project" element={<RegisterProject />} />
+    <Route path="ships" element={<Ships />} />
+    <Route path="game-rules" element={<GameRules />} />
+    <Route path="apply" element={<Apply />} />
+    <Route path="project/:id" element={<Project />} />
   </Routes>
 );
