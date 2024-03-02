50044499eb3a073902709394762dd5fd04604e3e
jordan <jordan.lesich@gmail.com>
2024-03-01 20:54:55 -0800

    under construction alert for empty tabs in ship dash

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -11,32 +11,10 @@ export const secondsToLongDateTime = (seconds: number) =>
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
-function abbreviateTimeAgo(timeAgo: string) {
-  const parts = timeAgo.split(' ');
-  if (parts.length === 2) {
-    const number = parts[0];
-    const unit = parts[1];
-
-    let abbreviation;
-    if (unit.startsWith('day')) abbreviation = 'd';
-    else if (unit.startsWith('hour')) abbreviation = 'h';
-    else if (unit.startsWith('minute')) abbreviation = 'm';
-    else if (unit.startsWith('second')) abbreviation = 's';
-    else if (unit.startsWith('month')) abbreviation = 'mo';
-    else if (unit.startsWith('year')) abbreviation = 'y';
-
-    return `${number}${abbreviation}`;
-  } else if (parts.length === 1 && parts[0] === 'a') {
-    const unit = parts[1];
-    const abbreviation = unit.charAt(0);
-    return `1${abbreviation}`;
-  }
-  return timeAgo;
-}
-
 export const secondsToShortRelativeTime = (seconds: number) => {
   return dayjs.utc(seconds * 1000).fromNow();
 };
+
 export const secondsToRelativeTime = (seconds: number) => {
   const ms = seconds * 1000;
   const now = dayjs();
