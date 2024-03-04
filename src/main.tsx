4ffd0a4a3c2affcbb601c7c4a88b7e3ace17ffaa
jordan <jordan.lesich@gmail.com>
2024-03-04 00:41:17 -0800

    update ABIs, constants

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import './index.css';
+import {
+  createGameManagerPool,
+  createGameManagerPoolProfile,
+} from './scripts/createGameManagerPool.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+createGameManagerPool();
