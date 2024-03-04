ef7556aaacc97c1b384a2059f97259ad9cac081a
jordan <jordan.lesich@gmail.com>
2024-03-04 00:42:56 -0800

    update pool ID

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 import './index.css';
-import {
-  createGameManagerPool,
-  createGameManagerPoolProfile,
-} from './scripts/createGameManagerPool.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-createGameManagerPool();
