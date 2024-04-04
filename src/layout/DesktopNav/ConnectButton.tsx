4751db2da9433c3802efafc7ae03832fbed322cf
jordan <jordan.lesich@gmail.com>
2024-04-04 15:44:26 -0700

    fix table chevron menu

--- src/layout/DesktopNav/ConnectButton.tsx
+++ src/layout/DesktopNav/ConnectButton.tsx
@@ -110,7 +110,7 @@ const IsConnected = ({ address }: { address: Address }) => {
               </Box>
             </Tooltip>
           ) : (
-            <Box pos="absolute" right={12}>
+            <Box pos="absolute" right={isTablet ? -4 : 12}>
               {menuOpen ? (
                 <IconChevronDown size={18} />
               ) : (
