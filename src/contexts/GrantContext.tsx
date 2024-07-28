c60337eaeacedc3dbe279c5d8fb7b20e46edd8bf
jordan <jordan.lesich@gmail.com>
2024-07-28 12:05:32 -0700

    grant context

---
+++ src/contexts/GrantContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, ReactNode } from 'react';
+import {
+  getGrant,
+  GrantQueryType,
+  ProjectGrant,
+  ShipGrant,
+} from '../queries/getGrant';
+import { useQuery } from '@tanstack/react-query';
+import { Content } from '@tiptap/react';
+
+export const GrantContext = createContext<GrantQueryType>({
+  project: null,
+  ship: null,
+  beacon: null,
+  applicationTemplate: null,
+  timeline: [],
+});
+
+export const GrantProvider = ({
+  children,
+  grantId,
+}: {
+  children: ReactNode;
+  grantId: string;
+}) => {
+  const {
+    data: grant,
+    isLoading: isLoadingGrant,
+    error: grantError,
+    refetch: refetchGrant,
+  } = useQuery({
+    queryKey: ['grant', grantId],
+    queryFn: () => getGrant(grantId),
+  });
+
+  return (
+    <GrantContext.Provider
+      value={{
+        project: (grant?.project as ProjectGrant) || null,
+        ship: (grant?.ship as ShipGrant) || null,
+        beacon: (grant?.beacon as Content) || null,
+        applicationTemplate: (grant?.applicationTemplate as Content) || null,
+        timeline: grant?.timeline || [],
+      }}
+    >
+      {children}
+    </GrantContext.Provider>
+  );
+};
--- src/queries/getGrant.ts
+++ src/queries/getGrant.ts
@@ -1,6 +1,36 @@
-import { getBuiltGraphSDK } from '../.graphclient';
+import { Content } from '@tiptap/react';
+import {
+  BaseShipDataFragment,
+  ProjectDataFragment,
+  getBuiltGraphSDK,
+} from '../.graphclient';
+import { beaconNotSubmitted, defaultApplication } from '../constants/copy';
 import { resolveShipMetadata } from '../resolvers/grantResolvers';
-import { resolveProjectMetadata } from '../resolvers/projectResolvers';
+import {
+  ProjectMetadata,
+  resolveProjectMetadata,
+} from '../resolvers/projectResolvers';
+import { ShipMetadata } from '../resolvers/shipResolvers';
+import { resolveRichTextMetadata } from '../resolvers/updates';
+
+export type ProjectGrant =
+  | (ProjectDataFragment & { metadata: ProjectMetadata | null })
+  | null;
+
+export type ShipGrant =
+  | (BaseShipDataFragment & { profileMetadata: ShipMetadata | null } & {
+      beaconMessage: { pointer: string } | null;
+      customApplication: { pointer: string } | null;
+    })
+  | null;
+
+export type GrantQueryType = {
+  project: ProjectGrant;
+  ship: ShipGrant;
+  beacon: Content;
+  applicationTemplate: Content;
+  timeline: any[];
+};
 
 export const getGrant = async (grantId: string) => {
   const { getGrant } = getBuiltGraphSDK();
@@ -20,28 +50,38 @@ export const getGrant = async (grantId: string) => {
 
   const {
     Project_by_pk: project,
-    GrantShip: ship,
+    GrantShip: ships,
     Grant_by_pk: grantData,
     Update: updates,
     Application: applications,
     MilestoneSet: milestonesSets,
   } = grant;
 
+  const ship = ships ? ships[0] : null;
+
   const [projectMetadata, shipMetadata] = await Promise.all([
     project ? await resolveProjectMetadata(project?.metadata?.pointer) : null,
-    ship ? await resolveShipMetadata(ship[0]?.profileMetadata?.pointer) : null,
+    ship ? await resolveShipMetadata(ship.profileMetadata?.pointer) : null,
   ]);
 
   const resolvedShip =
-    ship && shipMetadata ? { ...ship[0], profileMetadata: shipMetadata } : null;
+    ship && shipMetadata ? { ...ship, profileMetadata: shipMetadata } : null;
 
   const resolvedProject =
     project && projectMetadata
       ? { ...project, metadata: projectMetadata }
       : null;
+  const resolvedBeacon = ship?.beaconMessage?.pointer
+    ? await resolveRichTextMetadata(ship.beaconMessage.pointer)
+    : null;
+  const resolvedCustomApplication = ship?.customApplication?.pointer
+    ? await resolveRichTextMetadata(ship.customApplication.pointer)
+    : null;
 
   return {
     project: resolvedProject,
     ship: resolvedShip,
-  };
+    beacon: resolvedBeacon || beaconNotSubmitted,
+    applicationTemplate: resolvedCustomApplication || defaultApplication,
+  } as GrantQueryType;
 };
