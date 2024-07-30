efacad1c9ae150e5c0a56e165cf35a4e6c9d4406
jordan <jordan.lesich@gmail.com>
2024-07-30 09:24:36 -0700

    build milestone display, tx

--- src/components/grant/ApplicationDisplay.tsx
+++ src/components/grant/ApplicationDisplay.tsx
@@ -6,13 +6,9 @@ import {
   Spoiler,
   Stack,
   Text,
-  Textarea,
   useMantineTheme,
 } from '@mantine/core';
 import {
-  IconCheck,
-  IconChevronCompactDown,
-  IconChevronCompactUp,
   IconChevronDown,
   IconChevronUp,
   IconCircleCheck,
@@ -28,19 +24,12 @@ import { useGrant } from '../../hooks/useGrant';
 import { Content } from '@tiptap/react';
 import { RTDisplay } from '../RTDisplay';
 import { secondsToLongDate } from '../../utils/time';
-import { Address, formatEther } from 'viem';
-import { GAME_TOKEN, ZER0_ADDRESS } from '../../constants/gameSetup';
+import { formatEther } from 'viem';
+import { GAME_TOKEN } from '../../constants/gameSetup';
 import { SCAN_URL } from '../../constants/enpoints';
-import { GameStatus, GrantStatus } from '../../types/common';
-import { useInputState } from '@mantine/hooks';
-import { useTx } from '../../hooks/useTx';
-import { useState } from 'react';
-import { notifications } from '@mantine/notifications';
-import GrantShipAbi from '../../abi/GrantShip.json';
-import { Tag } from '../../constants/tags';
-import { reasonSchema } from '../../utils/ipfs/metadataValidation';
-import { pinJSONToIPFS } from '../../utils/ipfs/pin';
+import { GameStatus } from '../../types/common';
 import classes from '../../styles/Spoiler.module.css';
+import { ApplicationVerdictControls } from './ApplicationVerdictControls';
 
 export const ApplicationDisplay = ({
   amountRequested,
@@ -182,132 +171,10 @@ export const ApplicationDisplay = ({
           <RTDisplay content={rtContent} minified />
         )}
         {isShipOperator && status === GameStatus.Pending && (
-          <OperatorControls />
+          <ApplicationVerdictControls />
         )}
       </Box>
       <Divider mb="lg" />
-    </Box>
-  );
-};
-
-const OperatorControls = () => {
-  const { ship, project, refetchGrant } = useGrant();
-  const [reason, setReason] = useInputState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const { tx } = useTx();
-
-  const handleApprove = async (isApproved: boolean) => {
-    try {
-      setIsLoading(true);
-      if (!ship || !ship.shipContractAddress) {
-        notifications.show({
-          title: 'Error',
-          message: 'Ship not found',
-          color: 'red',
-        });
-        setIsLoading(false);
-        return;
-      }
-
-      if (!project) {
-        notifications.show({
-          title: 'Error',
-          message: 'Project not found',
-          color: 'red',
-        });
-        setIsLoading(false);
-        return;
-      }
-
-      const metadata = {
-        reason: reason,
-      };
-
-      const parsed = reasonSchema.safeParse(metadata);
-
-      if (!parsed.success) {
-        notifications.show({
-          title: 'Error',
-          message: 'Invalid metadata',
-          color: 'red',
-        });
-        setIsLoading(false);
-        return;
-      }
-
-      const pinRes = await pinJSONToIPFS(parsed.data);
-
-      if (typeof pinRes.IpfsHash !== 'string' && pinRes.IpfsHash[0] !== 'Q') {
-        notifications.show({
-          title: 'IPFS Upload Error',
-          message: pinRes.IpfsHash[1],
-          color: 'red',
-        });
-        setIsLoading(false);
-        return;
-      }
-
-      const TAG = `${Tag.ShipReviewGrant}:${project.id}:${isApproved ? GameStatus.Accepted : GameStatus.Rejected}`;
-
-      tx({
-        writeContractParams: {
-          abi: GrantShipAbi,
-          address: ship.shipContractAddress as Address,
-          functionName: 'postUpdate',
-          args: [TAG, [1n, pinRes.IpfsHash], ZER0_ADDRESS],
-        },
-        writeContractOptions: {
-          onPollSuccess() {
-            refetchGrant();
-            setIsLoading(false);
-          },
-          onError() {
-            setIsLoading(false);
-          },
-          onPollTimeout() {
-            setIsLoading(false);
-          },
-        },
-      });
-    } catch (error) {
-      console.error(error);
-      notifications.show({
-        title: 'Error',
-        message: 'Failed to approve grant',
-        color: 'red',
-      });
-      setIsLoading(false);
-    }
-  };
-
-  return (
-    <Box mt="sm">
-      <Textarea
-        value={reason}
-        onChange={setReason}
-        label="Reason"
-        placeholder="Provide constructive feedback and reasoning for your decision."
-        minRows={3}
-        autosize
-        required
-        mb="lg"
-      />
-      <Group justify="flex-end">
-        <Button
-          variant="secondary"
-          disabled={isLoading || !reason}
-          onClick={() => handleApprove(false)}
-        >
-          Not Approve
-        </Button>
-        <Button
-          variant="primary"
-          disabled={isLoading || !reason}
-          onClick={() => handleApprove(true)}
-        >
-          Approve
-        </Button>
-      </Group>
     </Box>
   );
 };
